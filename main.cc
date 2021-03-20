e288868a3a06d0119f116f6845058b77c0a60fef
phlc <pedrocarvalho@outlook.com>
2021-03-20 15:32:42 -0300

    trabalho1.cc -> graph_matrix.cc main fuction changed to main.cc

---
+++ main.cc
@@ -0,0 +1,37 @@
+/*
+Pucminas - Ciência da Computação - Coração Eucarístico
+Grafos - Manhã
+Pedro Henrique Lima Carvalho
+
+Desafio Finding Bridges
+- Deep Search First
+- Finding Bridges
+*/
+
+//Dependencies
+#include <iostream>
+#include "graph_matrix.cc"
+
+using namespace std;
+
+/*
+Main - Main Function
+*/
+int main(){
+    Valued_Graph grafo = Valued_Graph(5, false, true, 2);
+    grafo.print();
+    int v1 = grafo.addVertex();
+    grafo.print();
+    int v2 = grafo.addVertex();
+    grafo.print();
+    int a1 = grafo.addEdge(v1,v2, 4);
+    grafo.print();
+    int a2 = grafo.addEdge(v1,v2, 8);
+    grafo.print();
+    grafo.removeEdge(v1,v2,a1);
+    grafo.print();
+    int a3 = grafo.addEdge(v1,v2,16);
+    grafo.print();
+    grafo.removeVertex(v1);
+    grafo.print();
+}
